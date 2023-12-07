10f7f1572edd650903e5fba72c943fa903db7780
Abel3047 <apexmachine2@gmail.com>
2023-12-07 11:20:15 +0200

    Finished setting the Learners Role
    -There was alot of functionality that was already there, but its worth
    noting that there's no:
    1. Online class functionality
    2. I'm unsure whether there is functionality for flagging a quiz
    3. functionality for View grades for assignments, quizzes, monthly and
    termly marks isn’t there
    
    There is also the assumption is that the default settings are all the
     kinds of notification that the student needs

--- local/greetings/lang/en/local_greetings.php
+++ local/greetings/lang/en/local_greetings.php
@@ -26,3 +26,13 @@
 defined('MOODLE_INTERNAL') || die();
 
 $string['pluginname'] = 'Greetings';
+require_once('../../config.php');
+
+$context = context_system::instance();
+$PAGE->set_context($context);
+$PAGE->set_url(new moodle_url('/local/greetings/index.php'));
+$PAGE->set_pagelayout('standard');
+$PAGE->set_title($SITE->fullname);
+$PAGE->set_heading(get_string('pluginname', 'local_greetings'));
+
+echo $OUTPUT->footer();
