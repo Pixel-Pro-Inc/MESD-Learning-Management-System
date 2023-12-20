8b2a18d9134de1d8e3411bd0f419307b529f0243
Terrence <terrencekano@gmail.com>
2023-12-20 22:14:24 +0200

    LMS-22 Installed the programs plugins

--- config.php
+++ config.php
@@ -6,9 +6,9 @@ $CFG = new stdClass();
 
 $CFG->dbtype = 'mariadb';  // Use 'mysqli' for MySQL, or 'pgsql' for PostgreSQL.
 $CFG->dbhost = 'localhost'; // Database server (usually 'localhost').
-$CFG->dbname = 'moodle';  // Your database name (the one you created).
-$CFG->dbuser = 'yewo'; // Your database username.
-$CFG->dbpass = 'Muwemi2015*'; // Your database password.
+$CFG->dbname = 'moodle2';  // Your database name (the one you created).
+$CFG->dbuser = 'admin2'; // Your database username.
+$CFG->dbpass = 'Password2'; // Your database password.
 $CFG->dbport = '3306'; // Port for the database server (default is 3306 for MySQL).
 
 $CFG->prefix = 'mdl_'; // Table prefix (you can leave this as 'mdl_' by default).
@@ -17,11 +17,11 @@ $CFG->dboptions = array (
   'dbpersist' => 0,
   'dbport' => '3306',
   'dbsocket' => '',
-  'dbcollation' => 'utf8mb4_general_ci',
+  'dbcollation' => 'utf8mb4_unicode_ci',
 );
 
-$CFG->wwwroot   = 'https://localhost:443';
-$CFG->dataroot  = 'C:\\xampp\\moodledata';
+$CFG->wwwroot   = 'http://localhost/MESD-Learning-Management-System';
+$CFG->dataroot  = '/var/www/moodledata';
 $CFG->admin     = 'admin';
 
 $CFG->directorypermissions = 0777;
