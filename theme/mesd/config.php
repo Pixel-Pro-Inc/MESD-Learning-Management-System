6affec7ddb5487ab6db10421c30c3e80f136b67f
Yewo-Devs <85104248+Yewo-Devs@users.noreply.github.com>
2023-12-31 16:10:52 +0200

    Created custom theme

---
+++ theme/mesd/config.php
@@ -0,0 +1,31 @@
+<?php
+// This line protects the file from being accessed by a URL directly.                                                               
+defined('MOODLE_INTERNAL') || die();
+
+// $THEME is defined before this page is included and we can define settings by adding properties to this global object.            
+                                                                                                                                    
+// The first setting we need is the name of the theme. This should be the last part of the component name, and the same             
+// as the directory name for our theme.                                                                                             
+$THEME->name = 'mesd';                                                                                                             
+                                                                                                                                    
+// This setting list the style sheets we want to include in our theme. Because we want to use SCSS instead of CSS - we won't        
+// list any style sheets. If we did we would list the name of a file in the /style/ folder for our theme without any css file      
+// extensions.                                                                                                                      
+$THEME->sheets = ['styles'];
+
+// This is a critical setting. We want to inherit from theme_boost because it provides a great starting point for SCSS bootstrap4   
+// themes. We could add more than one parent here to inherit from multiple parents, and if we did they would be processed in        
+// order of importance (later themes overriding earlier ones). Things we will inherit from the parent theme include                 
+// styles and mustache templates and some (not all) settings.                                                                       
+$THEME->parents = ['boost'];                                                                                                        
+                                                                                                                                    
+// A dock is a way to take blocks out of the page and put them in a persistent floating area on the side of the page. Boost         
+// does not support a dock so we won't either - but look at bootstrapbase for an example of a theme with a dock.                    
+$THEME->enable_dock = false;                                                                                                        
+                                                                                                                                    
+// This is an old setting used to load specific CSS for some YUI JS. We don't need it in Boost based themes because Boost           
+// provides default styling for the YUI modules that we use. It is not recommended to use this setting anymore.                     
+$THEME->yuicssmodules = array();                                                                                                    
+                                                                                                                                    
+// Most themes will use this rendererfactory as this is the one that allows the theme to override any other renderer.               
+$THEME->rendererfactory = 'theme_overridden_renderer_factory';
