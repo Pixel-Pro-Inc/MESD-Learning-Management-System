bfadb044f901c67eba9be76c90fe4aaa07b522f5
Yewo-Devs <85104248+Yewo-Devs@users.noreply.github.com>
2024-01-14 18:27:48 +0200

    Updated plugin

--- mod/synchronouslearning/view.php
+++ mod/synchronouslearning/view.php
@@ -45,13 +45,14 @@ require_login($course, true, $cm);
 
 $modulecontext = context_module::instance($cm->id);
 
-$event = \mod_synchronouslearning\event\course_module_viewed::create(array(
-    'objectid' => $moduleinstance->id,
-    'context' => $modulecontext
-));
-$event->add_record_snapshot('course', $course);
-$event->add_record_snapshot('synchronouslearning', $moduleinstance);
-$event->trigger();
+//$event = \mod_synchronouslearning\event\course_module_viewed::create(array(
+//    'objectid' => $moduleinstance->id,
+//    'context' => $modulecontext
+//));
+//
+//$event->add_record_snapshot('course', $course);
+//$event->add_record_snapshot('synchronouslearning', $moduleinstance);
+//$event->trigger();
 
 $PAGE->set_url('/mod/synchronouslearning/view.php', array('id' => $cm->id));
 $PAGE->set_title(format_string($moduleinstance->name));
