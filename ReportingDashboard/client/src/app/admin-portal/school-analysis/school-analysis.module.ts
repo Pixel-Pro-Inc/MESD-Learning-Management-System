3f2122c21e3b71eeb65dcd355c6c63389f262f68
Abel Tshimbalanga <apexmachine2@gmail.com>
2024-01-23 15:55:57 +0200

    Set up the feature for school anaylsis
    -The module and component were set up but it isn't functional

---
+++ ReportingDashboard/client/src/app/admin-portal/school-analysis/school-analysis.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SchoolAnalysisComponent } from './school-analysis.component';
+
+@NgModule({
+  declarations: [SchoolAnalysisComponent],
+  imports: [
+    CommonModule,    
+  ]
+})
+export class SchoolAnalysisModule { }
