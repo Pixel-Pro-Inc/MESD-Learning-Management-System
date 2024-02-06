97790b2e5dd699fb6ae54cd00c4a7bb73cf7562b
Yewo-Devs <85104248+Yewo-Devs@users.noreply.github.com>
2024-02-06 06:16:32 +0200

    Updated repo

--- local/autologin/lang/en/local_autologin.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-defined('MOODLE_INTERNAL') || die();
-
-$string['eventuseradded'] = 'Auto Login';
-$string['pluginname'] = 'Auto Login';
--- local/autologin/version.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-defined('MOODLE_INTERNAL') || die();
-
-$plugin->version = 2023120400;
-$plugin->requires = 2020110900;
-$plugin->component = 'local_autologin';
-$plugin->maturity  = MATURITY_BETA;
