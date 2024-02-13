0bdc6eb67cfe8e7780abaf064dca31e69d75c6ce
Yewo-Devs <85104248+Yewo-Devs@users.noreply.github.com>
2024-02-14 01:33:52 +0200

    Rewrote redirerct api in c#

---
+++ RedirectAPI/API/Application/Interfaces/ILinkPreviewService.cs
@@ -0,0 +1,9 @@
+﻿using API.Core.Model;
+
+namespace API.Application.Interfaces
+{
+    public interface ILinkPreviewService
+    {
+        Task<LinkPreview> GetLinkPreviewAsync(string url);
+    }
+}
