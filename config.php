a0a9ff6d449c37c79cf9e8e4e6755be15dea8eef
Abel Tshimbalanga <apexmachine2@gmail.com>
2024-02-15 17:45:54 +0200

    stuff

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ $CFG = new stdClass();
 
 $CFG->dbtype    = 'mariadb';
 $CFG->dblibrary = 'native';
-$CFG->dbhost    = getenv('MOODLE_DB_HOST');
+$CFG->dbhost    = $_SERVER['MOODLE_DB_HOST'];
 $CFG->dbname    = 'moodle';
 $CFG->dbuser    = 'admin';
 $CFG->dbpass    = 'Password';
