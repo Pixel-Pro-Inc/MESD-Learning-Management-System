8d23e6e931863f8b0efaa7fc8b02bf9a53c3213b
Yewo-Devs <85104248+Yewo-Devs@users.noreply.github.com>
2024-03-10 14:10:01 +0200

    Updated entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-# Export environment variables from /proc/1/environ to /etc/environment
-while IFS= read -r line; do
-    echo "$line" >> /etc/environment
-done < /proc/1/environ
-exec "$@"
+printenv | grep -v "no_proxy" >> /etc/environment
