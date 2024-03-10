a46ba5b3d9e19a0cd65003a99de3f25eb2fcfd9e
Yewo-Devs <85104248+Yewo-Devs@users.noreply.github.com>
2024-03-10 11:51:29 +0200

    Updated cron

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-# Export environment variables to a file
-printenv | grep -v "no_proxy" >> /etc/environment
+# Export environment variables from /proc/1/environ to /etc/environment
+while IFS= read -r line; do
+    echo "$line" >> /etc/environment
+done < /proc/1/environ
 exec "$@"
