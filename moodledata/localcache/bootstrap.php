3a9f4ea3e189513ce8b8875c5f27da9aedffcb46
Yewo-Devs <85104248+Yewo-Devs@users.noreply.github.com>
2024-05-21 17:07:28 +0200

    Deleted cache files

--- moodledata/localcache/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-// ********** This file is generated DO NOT EDIT **********
-$CFG->siteidentifier = 'v0C5VOpPkY8EP96ti6c9mDQMqkygyugGec2-51-21-127-249.eu-north-1.compute.amazonaws.com';
-$CFG->bootstraphash = '490515dd21290335903e1a2bdc8c3a4c';
-// Only if the file is not stale and has not been defined.
-if ($CFG->bootstraphash === hash_local_config_cache() && !defined('SYSCONTEXTID')) {
-    define('SYSCONTEXTID', 1);
-}
