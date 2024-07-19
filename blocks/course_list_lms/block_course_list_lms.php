4428992e28122669187cf3d8c62028ccc26ed23a
Yewo-Devs <85104248+Yewo-Devs@users.noreply.github.com>
2024-07-19 13:23:20 +0200

    Update courses

--- blocks/course_list_lms/block_course_list_lms.php
+++ blocks/course_list_lms/block_course_list_lms.php
@@ -52,7 +52,7 @@ class block_course_list_lms extends block_list {
     
         if ($courses) {
             foreach ($courses as $course) {
-                if($course->id !== 1){
+                if($course->id != 1){
                     // Find the custom course field with id 5 for this course
                 $customfield = $DB->get_record('customfield_data', array('instanceid' => $course->id, 'fieldid' => 5));
     
